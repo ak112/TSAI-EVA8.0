959459e227cf1c25411ada68af81831f3f666359
Akash Bhadauria <akashbhadauria196@gmail.com>
2023-02-06 00:25:38 +0530

    Updating model

--- 06_AdvancedConvolution/model.py
+++ 06_AdvancedConvolution/model.py
@@ -6,50 +6,124 @@ class Cifar10Net(nn.Module):
     def __init__(self):
         super(Cifar10Net, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 32, 3, stride=2, padding=1),
-            nn.BatchNorm2d(32),
+            
+            nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False),
             nn.ReLU(),
+            nn.BatchNorm2d(64),
             nn.Dropout(0.1),
         )
+
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 32, 3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            
+            nn.Conv2d(64, 64, 3, stride=1, padding=0, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
             nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=0, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=0, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
             nn.Dropout(0.1),
         )
+
         self.conv3 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
             nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, dilation=2, stride=2, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
             nn.Dropout(0.1),
         )
-        self.dw_conv = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64),
-            nn.BatchNorm2d(64),
+
+        self.conv4 = nn.Sequential(
+            
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
             nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, dilation=2, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
             nn.Dropout(0.1),
         )
-        self.dilated_conv = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, dilation=2, padding=2),
-            nn.BatchNorm2d(64),
+
+        self.conv5 = nn.Sequential(
+            
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
             nn.ReLU(),
-            nn.Dropout(0.1)
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 64, 1, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 64, 3, dilation=2, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 10, 3, stride=1, padding=0, bias=False),
         )
-        self.gap = nn.Sequential (
-            nn.AdaptiveAvgPool2d((1, 1))
+
+        self.gap = nn.Sequential(
+            
+            nn.AdaptiveAvgPool2d(1),
         )
-        self.fc = nn.Linear(64, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = self.dw_conv(x)
-        x = self.dilated_conv(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
         x = self.gap(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
+
+        x = x.view(-1, 10)
+
+        return F.log_softmax(x, dim=1)
 
 
 
