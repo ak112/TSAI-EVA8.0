322f01508760c076df0ad4e68346b586b20c14b6
Akash Bhadauria <akashbhadauria196@gmail.com>
2023-02-05 18:00:47 +0530

    Updating model

--- 06_AdvancedConvolution/model.py
+++ 06_AdvancedConvolution/model.py
@@ -2,15 +2,49 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Cifar10Net(nn.Module):
     def __init__(self, num_classes=10):
         super(Cifar10Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
-        self.dw_conv = nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64)
-        self.dilated_conv = nn.Conv2d(64, 64, 3, stride=1, dilation=2, padding=2)
-        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, stride=2, padding=1,bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, stride=2, padding=1,bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, stride=2, padding=1,bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+        )
+
+
+        self.dw_conv = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64,bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+        )
+
+        self.dilated_conv = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=1, dilation=2, padding=2,bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+        )
+
+        self.gap = nn.Sequential (
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
         self.fc = nn.Linear(64, num_classes)
 
     def forward(self, x):
