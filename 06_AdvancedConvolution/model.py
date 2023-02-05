1eb6e0372e7c86c4ec0e565858a026fe6e51fc3d
Akash Bhadauria <akashbhadauria196@gmail.com>
2023-02-05 22:03:22 +0530

    Updating model

--- 06_AdvancedConvolution/model.py
+++ 06_AdvancedConvolution/model.py
@@ -2,57 +2,54 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class Cifar10Net(nn.Module):
     def __init__(self):
         super(Cifar10Net, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 32, 3, stride=2, padding=1,bias=False),
-            nn.ReLU(),
+            nn.Conv2d(3, 32, 3, stride=2, padding=1),
             nn.BatchNorm2d(32),
-            nn.Dropout(0.1)
+            nn.ReLU(),
+            nn.Dropout(0.1),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 32, 3, stride=2, padding=1,bias=False),
-            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1, padding=1),
             nn.BatchNorm2d(32),
+            nn.ReLU(),
             nn.Dropout(0.1),
         )
-
         self.conv3 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, stride=2, padding=1,bias=False),
-            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),
             nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.Dropout(0.1),
         )
-
-
         self.dw_conv = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64,bias=False),
-            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, groups=64),
             nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.Dropout(0.1),
         )
-
         self.dilated_conv = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, dilation=2, padding=2,bias=False),
-            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1, dilation=2, padding=2),
             nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(0.1)
         )
-
         self.gap = nn.Sequential (
             nn.AdaptiveAvgPool2d((1, 1))
         )
-
-        self.fc = nn.Linear(64,10)
+        self.fc = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.dw_conv(x))
-        x = F.relu(self.dilated_conv(x))
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.dw_conv(x)
+        x = self.dilated_conv(x)
         x = self.gap(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+
+
